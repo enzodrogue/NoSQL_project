cb19afcb585967d00433d40bea8c071dc51a0f80
bjtbjt <benoit.harbeumont@gmail.com>
2015-11-20 15:55:47 +0100

    mongoclient

--- src/main/java/MongoDbClient.java
+++ src/main/java/MongoDbClient.java
@@ -1,7 +1,10 @@
 
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
+import java.time.temporal.JulianFields;
 import java.util.Arrays;
 
 public class MongoDbClient {
@@ -12,6 +15,10 @@ public class MongoDbClient {
         //MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
         MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
         MongoClient mongoClient = new MongoClient(connectionString);
-        MongoDatabase database = mongoClient.getDatabase("DBLP");
+        MongoDatabase database = mongoClient.getDatabase("PROJ");
+
+        MongoCollection<Document> collection = database.getCollection("proj");
+        System.out.print(collection.count());
+
     }
 }
